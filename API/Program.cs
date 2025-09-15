7e5d2464404655740db66921e50d7a84d07acbcb
Lee Tae Heon <thhoney218@gmail.com>
2025-09-15 22:31:19 +1000

    React Basics

--- API/Program.cs
+++ API/Program.cs
@@ -9,8 +9,14 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
 
+app.UseCors(x =>
+{
+    x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3001");
+}
+);
 
 app.MapControllers();
 
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,66 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import type { Product } from "../../product";
+
+type Props = {
+  product: Product;
+};
+
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card
+      elevation={3}
+      sx={{
+        width: 280,
+        borderRadius: 2,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        transition: "all .15s ease",
+        "&:hover": {
+          boxShadow: 6,
+          transform: "translateY(-2px)",
+        },
+      }}
+    >
+      <CardMedia
+        sx={{
+          height: 240,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="subtitle2"
+          sx={{ textTransform: "uppercase", letterSpacing: 0.5 }}
+        >
+          {product.name}
+        </Typography>
+
+        <Typography variant="h6" color="secondary" sx={{ fontWeight: 600 }}>
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+      </CardContent>
+
+      <CardActions sx={{ justifyContent: "space-between", px: 2, pb: 2 }}>
+        <Button size="small" variant="outlined">
+          View
+        </Button>
+        <Button size="small" variant="contained">
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
