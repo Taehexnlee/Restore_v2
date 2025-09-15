fbd518222cf7608c2cb8ce20c3e90e1f6a9585f6
Lee Tae Heon <thhoney218@gmail.com>
2025-09-15 15:39:48 +1000

    API basics

---
+++ API/Program.cs
@@ -0,0 +1,18 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+
+app.MapControllers();
+
+DbInitializer.InitDb(app);
+app.Run();
